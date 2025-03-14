db25c6e683f5508469141e671820d81b833dca61
MaksatDowletow <tdlipfmdm@outlook.com>
2025-03-15 01:32:08 +0500

    Create saveWord.js

---
+++ saveWord.js
@@ -0,0 +1,36 @@
+document.getElementById('saveWord').addEventListener('click', () => {
+    const content = document.getElementById('result').innerHTML;
+
+    const zip = new PizZip();
+    const doc = new window.docxtemplater(zip, {
+        paragraphLoop: true,
+        linebreaks: true,
+    });
+
+    const template = `
+    <html>
+        <head>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            ${content}
+        </body>
+    </html>
+    `;
+
+    zip.file("word/document.xml", template);
+    doc.loadZip(zip);
+
+    try {
+        doc.render();
+    } catch (error) {
+        console.error(error);
+    }
+
+    const out = doc.getZip().generate({
+        type: "blob",
+        mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    });
+
+    saveAs(out, "document.docx");
+});

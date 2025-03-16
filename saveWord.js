72e5246f2ed3fc40533dc6a5a50c51a5197f4256
MaksatDowletow <tdlipfmdm@outlook.com>
2025-03-16 22:44:04 +0500

    Update saveWord.js

--- saveWord.js
+++ saveWord.js
@@ -1,42 +1,28 @@
 document.getElementById("saveWord").addEventListener("click", () => {
+    if (typeof Docxtemplater === "undefined") {
+        console.error("Docxtemplater не загружен!");
+        return;
+    }
+
     const content = `
         <w:document xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
             <w:body>
-                <w:p>
-                    <w:r>
-                        <w:t>${document.getElementById("date").value}</w:t>
-                    </w:r>
-                </w:p>
-                <w:p>
-                    <w:r>
-                        <w:t>${document.getElementById("fname").value}</w:t>
-                    </w:r>
-                </w:p>
-                <w:p>
-                    <w:r>
-                        <w:t>${document.getElementById("department").value}</w:t>
-                    </w:r>
-                </w:p>
-                <w:p>
-                    <w:r>
-                        <w:t>${document.getElementById("conclusion").value}</w:t>
-                    </w:r>
-                </w:p>
-                <w:p>
-                    <w:r>
-                        <w:t>${document.getElementById("doctor").value}</w:t>
-                    </w:r>
-                </w:p>
+                <w:p><w:r><w:t>Senesi: ${document.getElementById("date").value}</w:t></w:r></w:p>
+                <w:p><w:r><w:t>Familiýasy, ady: ${document.getElementById("fname").value}</w:t></w:r></w:p>
+                <w:p><w:r><w:t>Bölüm: ${document.getElementById("department").value}</w:t></w:r></w:p>
+                <w:p><w:r><w:t>Netije: ${document.getElementById("conclusion").value}</w:t></w:r></w:p>
+                <w:p><w:r><w:t>Lukman: ${document.getElementById("doctor").value}</w:t></w:r></w:p>
             </w:body>
         </w:document>
     `;
 
     const zip = new PizZip();
     zip.file("word/document.xml", content);
-    const doc = new Docxtemplater(zip, { paragraphLoop: true, linebreaks: true });
 
     try {
+        const doc = new Docxtemplater(zip);
         doc.render();
+
         const out = doc.getZip().generate({
             type: "blob",
             mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
